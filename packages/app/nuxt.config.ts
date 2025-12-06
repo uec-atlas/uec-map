d58bd8e156f751296d81e2c22df4215cb1f4771c
Sota Iguchi (e-chan1007) <echan041007@gmail.com>
2025-12-06 15:27:18 +0900

    ci: デプロイ設定

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: "2024-09-19",
   devtools: { enabled: true },
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
   modules: [
     "@vueuse/nuxt",
     "@nuxt/ui",
