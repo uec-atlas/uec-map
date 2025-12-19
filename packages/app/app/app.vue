94d3d2976043d7c4c4343414d3681ffc9bcd62fd
Sota Iguchi (e-chan1007) <echan041007@gmail.com>
2025-12-20 00:23:24 +0900

    chore: ページタイトルを設定

--- packages/app/app/app.vue
+++ packages/app/app/app.vue
@@ -1,13 +1,12 @@
 <template>
+  <Head>
+    <Title>UEC学内マップ</Title>
+    <Meta
+      name="description"
+      content="UEC学内マップは、電気通信大学のキャンパス内の建物や施設の情報を提供する地図アプリケーションです。"
+    />
+  </Head>
   <UApp>
-    <NuxtPage />
+    <NuxtPage/>
   </UApp>
 </template>
-
-<style lang="scss">
-html, body, #__nuxt {
-  width: 100dvw;
-  height: 100dvh;
-  margin: 0;
-}
-</style>
